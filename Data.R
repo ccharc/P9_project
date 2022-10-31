c6e75d5c14d5a935f7f1afb67104b373d6ec6c38
josandan <josandanr@gmail.com>
2022-10-31 11:08:05 +0100

    gitignore + data script

---
+++ Data.R
@@ -0,0 +1,183 @@
+AUDCAD= rbind(read.csv("DAT_NT_AUDCAD_T_LAST_201801.csv", sep=";", header = FALSE),
+               read.csv("DAT_NT_AUDCAD_T_LAST_201802.csv", sep=";", header = FALSE),
+               read.csv("DAT_NT_AUDCAD_T_LAST_201803.csv", sep=";", header = FALSE))
+
+AUDCHF= rbind(read.csv("DAT_NT_AUDCHF_T_LAST_201801.csv", sep=";", header = FALSE),
+               read.csv("DAT_NT_AUDCHF_T_LAST_201802.csv", sep=";", header = FALSE),
+               read.csv("DAT_NT_AUDCHF_T_LAST_201803.csv", sep=";", header = FALSE))
+
+AUDJPY= rbind(read.csv("DAT_NT_AUDJPY_T_LAST_201801.csv", sep=";", header = FALSE),
+              read.csv("DAT_NT_AUDJPY_T_LAST_201802.csv", sep=";", header = FALSE),
+              read.csv("DAT_NT_AUDJPY_T_LAST_201803.csv", sep=";", header = FALSE))
+
+AUDNZD= rbind(read.csv("DAT_NT_AUDNZD_T_LAST_201801.csv", sep=";", header = FALSE),
+              read.csv("DAT_NT_AUDNZD_T_LAST_201802.csv", sep=";", header = FALSE),
+              read.csv("DAT_NT_AUDNZD_T_LAST_201803.csv", sep=";", header = FALSE))
+
+AUDUSD= rbind(read.csv("DAT_NT_AUDUSD_T_LAST_201801.csv", sep=";", header = FALSE),
+              read.csv("DAT_NT_AUDUSD_T_LAST_201802.csv", sep=";", header = FALSE),
+              read.csv("DAT_NT_AUDUSD_T_LAST_201803.csv", sep=";", header = FALSE))
+
+CADCHF= rbind(read.csv("DAT_NT_CADCHF_T_LAST_201801.csv", sep=";", header = FALSE),
+              read.csv("DAT_NT_CADCHF_T_LAST_201802.csv", sep=";", header = FALSE),
+              read.csv("DAT_NT_CADCHF_T_LAST_201803.csv", sep=";", header = FALSE))
+
+CADJPY= rbind(read.csv("DAT_NT_CADJPY_T_LAST_201801.csv", sep=";", header = FALSE),
+              read.csv("DAT_NT_CADJPY_T_LAST_201802.csv", sep=";", header = FALSE),
+              read.csv("DAT_NT_CADJPY_T_LAST_201803.csv", sep=";", header = FALSE))
+
+CHFJPY= rbind(read.csv("DAT_NT_CHFJPY_T_LAST_201801.csv", sep=";", header = FALSE),
+              read.csv("DAT_NT_CHFJPY_T_LAST_201802.csv", sep=";", header = FALSE),
+              read.csv("DAT_NT_CHFJPY_T_LAST_201803.csv", sep=";", header = FALSE))
+
+EURAUD= rbind(read.csv("DAT_NT_EURAUD_T_LAST_201801.csv", sep=";", header = FALSE),
+              read.csv("DAT_NT_EURAUD_T_LAST_201802.csv", sep=";", header = FALSE),
+              read.csv("DAT_NT_EURAUD_T_LAST_201803.csv", sep=";", header = FALSE))
+
+EURCAD= rbind(read.csv("DAT_NT_EURCAD_T_LAST_201801.csv", sep=";", header = FALSE),
+              read.csv("DAT_NT_EURCAD_T_LAST_201802.csv", sep=";", header = FALSE),
+              read.csv("DAT_NT_EURCAD_T_LAST_201803.csv", sep=";", header = FALSE))
+
+EURCHF= rbind(read.csv("DAT_NT_EURCHF_T_LAST_201801.csv", sep=";", header = FALSE),
+              read.csv("DAT_NT_EURCHF_T_LAST_201802.csv", sep=";", header = FALSE),
+              read.csv("DAT_NT_EURCHF_T_LAST_201803.csv", sep=";", header = FALSE))
+
+EURCZK= rbind(read.csv("DAT_NT_EURCZK_T_LAST_201801.csv", sep=";", header = FALSE),
+              read.csv("DAT_NT_EURCZK_T_LAST_201802.csv", sep=";", header = FALSE),
+              read.csv("DAT_NT_EURCZK_T_LAST_201803.csv", sep=";", header = FALSE))
+
+EURGBP= rbind(read.csv("DAT_NT_EURGBP_T_LAST_201801.csv", sep=";", header = FALSE),
+              read.csv("DAT_NT_EURGBP_T_LAST_201802.csv", sep=";", header = FALSE),
+              read.csv("DAT_NT_EURGBP_T_LAST_201803.csv", sep=";", header = FALSE))
+
+EURHUF= rbind(read.csv("DAT_NT_EURHUF_T_LAST_201801.csv", sep=";", header = FALSE),
+              read.csv("DAT_NT_EURHUF_T_LAST_201802.csv", sep=";", header = FALSE),
+              read.csv("DAT_NT_EURHUF_T_LAST_201803.csv", sep=";", header = FALSE))
+
+EURJPY= rbind(read.csv("DAT_NT_EURJPY_T_LAST_201801.csv", sep=";", header = FALSE),
+              read.csv("DAT_NT_EURJPY_T_LAST_201802.csv", sep=";", header = FALSE),
+              read.csv("DAT_NT_EURJPY_T_LAST_201803.csv", sep=";", header = FALSE))
+
+EURNOK= rbind(read.csv("DAT_NT_EURNOK_T_LAST_201801.csv", sep=";", header = FALSE),
+              read.csv("DAT_NT_EURNOK_T_LAST_201802.csv", sep=";", header = FALSE),
+              read.csv("DAT_NT_EURNOK_T_LAST_201803.csv", sep=";", header = FALSE))
+
+EURNZD= rbind(read.csv("DAT_NT_EURNZD_T_LAST_201801.csv", sep=";", header = FALSE),
+              read.csv("DAT_NT_EURNZD_T_LAST_201802.csv", sep=";", header = FALSE),
+              read.csv("DAT_NT_EURNZD_T_LAST_201803.csv", sep=";", header = FALSE))
+
+EURPLN= rbind(read.csv("DAT_NT_EURPLN_T_LAST_201801.csv", sep=";", header = FALSE),
+              read.csv("DAT_NT_EURPLN_T_LAST_201802.csv", sep=";", header = FALSE),
+              read.csv("DAT_NT_EURPLN_T_LAST_201803.csv", sep=";", header = FALSE))
+
+EURSEK= rbind(read.csv("DAT_NT_EURSEK_T_LAST_201801.csv", sep=";", header = FALSE),
+              read.csv("DAT_NT_EURSEK_T_LAST_201802.csv", sep=";", header = FALSE),
+              read.csv("DAT_NT_EURSEK_T_LAST_201803.csv", sep=";", header = FALSE))
+
+EURTRY= rbind(read.csv("DAT_NT_EURTRY_T_LAST_201801.csv", sep=";", header = FALSE),
+              read.csv("DAT_NT_EURTRY_T_LAST_201802.csv", sep=";", header = FALSE),
+              read.csv("DAT_NT_EURTRY_T_LAST_201803.csv", sep=";", header = FALSE))
+
+EURUSD= rbind(read.csv("DAT_NT_EURUSD_T_LAST_201801.csv", sep=";", header = FALSE),
+              read.csv("DAT_NT_EURUSD_T_LAST_201802.csv", sep=";", header = FALSE),
+              read.csv("DAT_NT_EURUSD_T_LAST_201803.csv", sep=";", header = FALSE))
+
+GBPAUD= rbind(read.csv("DAT_NT_GBPAUD_T_LAST_201801.csv", sep=";", header = FALSE),
+              read.csv("DAT_NT_GBPAUD_T_LAST_201802.csv", sep=";", header = FALSE),
+              read.csv("DAT_NT_GBPAUD_T_LAST_201803.csv", sep=";", header = FALSE))
+
+GBPCAD= rbind(read.csv("DAT_NT_GBPCAD_T_LAST_201801.csv", sep=";", header = FALSE),
+              read.csv("DAT_NT_GBPCAD_T_LAST_201802.csv", sep=";", header = FALSE),
+              read.csv("DAT_NT_GBPCAD_T_LAST_201803.csv", sep=";", header = FALSE))
+
+GBPCHF= rbind(read.csv("DAT_NT_GBPCHF_T_LAST_201801.csv", sep=";", header = FALSE),
+              read.csv("DAT_NT_GBPCHF_T_LAST_201802.csv", sep=";", header = FALSE),
+              read.csv("DAT_NT_GBPCHF_T_LAST_201803.csv", sep=";", header = FALSE))
+
+GBPJPY= rbind(read.csv("DAT_NT_GBPJPY_T_LAST_201801.csv", sep=";", header = FALSE),
+              read.csv("DAT_NT_GBPJPY_T_LAST_201802.csv", sep=";", header = FALSE),
+              read.csv("DAT_NT_GBPJPY_T_LAST_201803.csv", sep=";", header = FALSE))
+
+GBPNZD= rbind(read.csv("DAT_NT_GBPNZD_T_LAST_201801.csv", sep=";", header = FALSE),
+              read.csv("DAT_NT_GBPNZD_T_LAST_201802.csv", sep=";", header = FALSE),
+              read.csv("DAT_NT_GBPNZD_T_LAST_201803.csv", sep=";", header = FALSE))
+
+GBPUSD= rbind(read.csv("DAT_NT_GBPUSD_T_LAST_201801.csv", sep=";", header = FALSE),
+              read.csv("DAT_NT_GBPUSD_T_LAST_201802.csv", sep=";", header = FALSE),
+              read.csv("DAT_NT_GBPUSD_T_LAST_201803.csv", sep=";", header = FALSE))
+
+NZDCAD= rbind(read.csv("DAT_NT_NZDCAD_T_LAST_201801.csv", sep=";", header = FALSE),
+              read.csv("DAT_NT_NZDCAD_T_LAST_201802.csv", sep=";", header = FALSE),
+              read.csv("DAT_NT_NZDCAD_T_LAST_201803.csv", sep=";", header = FALSE))
+
+NZDCHF= rbind(read.csv("DAT_NT_NZDCHF_T_LAST_201801.csv", sep=";", header = FALSE),
+              read.csv("DAT_NT_NZDCHF_T_LAST_201802.csv", sep=";", header = FALSE),
+              read.csv("DAT_NT_NZDCHF_T_LAST_201803.csv", sep=";", header = FALSE))
+
+NZDJPY= rbind(read.csv("DAT_NT_NZDJPY_T_LAST_201801.csv", sep=";", header = FALSE),
+              read.csv("DAT_NT_NZDJPY_T_LAST_201802.csv", sep=";", header = FALSE),
+              read.csv("DAT_NT_NZDJPY_T_LAST_201803.csv", sep=";", header = FALSE))
+
+NZDUSD= rbind(read.csv("DAT_NT_NZDUSD_T_LAST_201801.csv", sep=";", header = FALSE),
+              read.csv("DAT_NT_NZDUSD_T_LAST_201802.csv", sep=";", header = FALSE),
+              read.csv("DAT_NT_NZDUSD_T_LAST_201803.csv", sep=";", header = FALSE))
+
+SGDJPY= rbind(read.csv("DAT_NT_SGDJPY_T_LAST_201801.csv", sep=";", header = FALSE),
+              read.csv("DAT_NT_SGDJPY_T_LAST_201802.csv", sep=";", header = FALSE),
+              read.csv("DAT_NT_SGDJPY_T_LAST_201803.csv", sep=";", header = FALSE))
+
+USDCAD= rbind(read.csv("DAT_NT_USDCAD_T_LAST_201801.csv", sep=";", header = FALSE),
+              read.csv("DAT_NT_USDCAD_T_LAST_201802.csv", sep=";", header = FALSE),
+              read.csv("DAT_NT_USDCAD_T_LAST_201803.csv", sep=";", header = FALSE))
+
+USDCHF= rbind(read.csv("DAT_NT_USDCHF_T_LAST_201801.csv", sep=";", header = FALSE),
+              read.csv("DAT_NT_USDCHF_T_LAST_201802.csv", sep=";", header = FALSE),
+              read.csv("DAT_NT_USDCHF_T_LAST_201803.csv", sep=";", header = FALSE))
+
+USDCZK= rbind(read.csv("DAT_NT_USDCZK_T_LAST_201801.csv", sep=";", header = FALSE),
+              read.csv("DAT_NT_USDCZK_T_LAST_201802.csv", sep=";", header = FALSE),
+              read.csv("DAT_NT_USDCZK_T_LAST_201803.csv", sep=";", header = FALSE))
+
+USDHUF= rbind(read.csv("DAT_NT_USDHUF_T_LAST_201801.csv", sep=";", header = FALSE),
+              read.csv("DAT_NT_USDHUF_T_LAST_201802.csv", sep=";", header = FALSE),
+              read.csv("DAT_NT_USDHUF_T_LAST_201803.csv", sep=";", header = FALSE))
+
+USDJPY= rbind(read.csv("DAT_NT_USDJPY_T_LAST_201801.csv", sep=";", header = FALSE),
+              read.csv("DAT_NT_USDJPY_T_LAST_201802.csv", sep=";", header = FALSE),
+              read.csv("DAT_NT_USDJPY_T_LAST_201803.csv", sep=";", header = FALSE))
+
+USDNOK= rbind(read.csv("DAT_NT_USDNOK_T_LAST_201801.csv", sep=";", header = FALSE),
+              read.csv("DAT_NT_USDNOK_T_LAST_201802.csv", sep=";", header = FALSE),
+              read.csv("DAT_NT_USDNOK_T_LAST_201803.csv", sep=";", header = FALSE))
+
+USDPLN= rbind(read.csv("DAT_NT_USDPLN_T_LAST_201801.csv", sep=";", header = FALSE),
+              read.csv("DAT_NT_USDPLN_T_LAST_201802.csv", sep=";", header = FALSE),
+              read.csv("DAT_NT_USDPLN_T_LAST_201803.csv", sep=";", header = FALSE))
+
+USDSEK= rbind(read.csv("DAT_NT_USDSEK_T_LAST_201801.csv", sep=";", header = FALSE),
+              read.csv("DAT_NT_USDSEK_T_LAST_201802.csv", sep=";", header = FALSE),
+              read.csv("DAT_NT_USDSEK_T_LAST_201803.csv", sep=";", header = FALSE))
+
+USDSGD= rbind(read.csv("DAT_NT_USDSGD_T_LAST_201801.csv", sep=";", header = FALSE),
+              read.csv("DAT_NT_USDSGD_T_LAST_201802.csv", sep=";", header = FALSE),
+              read.csv("DAT_NT_USDSGD_T_LAST_201803.csv", sep=";", header = FALSE))
+
+USDTRY= rbind(read.csv("DAT_NT_USDTRY_T_LAST_201801.csv", sep=";", header = FALSE),
+              read.csv("DAT_NT_USDTRY_T_LAST_201802.csv", sep=";", header = FALSE),
+              read.csv("DAT_NT_USDTRY_T_LAST_201803.csv", sep=";", header = FALSE))
+
+USDZAR= rbind(read.csv("DAT_NT_USDZAR_T_LAST_201801.csv", sep=";", header = FALSE),
+              read.csv("DAT_NT_USDZAR_T_LAST_201802.csv", sep=";", header = FALSE),
+              read.csv("DAT_NT_USDZAR_T_LAST_201803.csv", sep=";", header = FALSE))
+
+ZARJPY= rbind(read.csv("DAT_NT_ZARJPY_T_LAST_201801.csv", sep=";", header = FALSE),
+              read.csv("DAT_NT_ZARJPY_T_LAST_201802.csv", sep=";", header = FALSE),
+              read.csv("DAT_NT_ZARJPY_T_LAST_201803.csv", sep=";", header = FALSE))
+
+
+plot(AUDCAD[,2], type ="l")
+plot(AUDCHF[,2], type ="l")
+plot(AUDJPY[,2], type ="l")
+plot(AUDNZD[,2], type ="l")
+
+
