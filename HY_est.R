0655db5e0e686bb1fdbc8b671760f50440aed368
christiansoendergaard <csan18@student.aau.dk>
2022-11-08 15:17:37 +0100

    Update HY_est.R

--- HY_est.R
+++ HY_est.R
@@ -16,7 +16,8 @@ theta = 0.8
 t_max = 1
 n = 28801                       # 8 hours in seconds
 gamma = sqrt(0.01)              # levels of volatility: 0, 0.001, 0.01
-lambda1 = c(3, 5, 10, 30, 60)   # avg. waiting times until new price
+# lambda1 = c(3, 5, 10, 30, 60)   # avg. waiting times until new price
+lambda1 = c(2, 4, 7, 10, 15)
 lambda2 = lambda1*2
 set.seed(101)
 
