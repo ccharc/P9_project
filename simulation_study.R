edd260d365bb19283172e47d481bbc033a7e3d62
josandan <josandanr@gmail.com>
2022-11-15 11:14:17 +0100

    small changes MRC

--- simulation_study.R
+++ simulation_study.R
@@ -1,59 +1,42 @@
 library(ggplot2)
 library(dplyr)
 library(highfrequency)
-library(xts)
 library(data.table)
-library(furrr)
-library(parallel)
-library(pbmcapply)
-
-# configs 
-X0 = 1
-b = 0.03
-beta0 = -5/16
-beta1 = 1/8
-alpha = -1/40
-rho = -0.3
-t_max = 1
-n = 28801                       # 8 hours in seconds
-gamma = sqrt(0.01)              # levels of volatility: 0, 0.001, 0.01
-lambda1 = c(3, 5, 10, 30, 60)   # avg. waiting times until new price
-#lambda1 = c(2, 2, 2, 2, 2)
-lambda2 = lambda1*2
-theta = 0.8
-#delta = 0.1
-#n_assets = 5
+# library(pbmcapply)
+source("config.R")
 R.utils::sourceDirectory("functions", modifiedOnly = FALSE)
 
 
-sim_study = function(n_assets){
+MRC_monte_carlo = function(seed){
+  
+  set.seed(seed)
+  
   # W needs to be specified outside simulate_price
   W_increments = sqrt(t_max/n)*rnorm(n+1,0,1)
   W = c(0,cumsum(W_increments))
 
   prices_and_sigmas = purrr::map(.x = rep(n, n_assets), .f = simulate_price, W = W)
-  price_list = purrr::map(prices_and_sigmas, .f = 1)
-  sigma_list = purrr::map(prices_and_sigmas, .f = 2)
+  price_list = purrr::map(prices_and_sigmas, .f = "price")
+  sigma_list = purrr::map(prices_and_sigmas, .f = "sigma")
   df_prices = refreshTime(price_list)
 
   df_prices = purrr::map_dfc(
-    .x = paste0("V", 1:n_assets), 
+    .x = paste0("V", 1:n_assets) %>% setNames(paste0("V", 1:n_assets)), 
     .f = function(x) {df_prices %>% dplyr::pull(x) %>% log()}
-    )%>% 
-    setNames(paste0("V", 1:n_assets)) %>%
+    ) %>%
     as.data.table() 
-
-
 
   # The pre-averaging window
   kn = floor(theta * nrow(df_prices)^(1/2))
 
   # The MRC estimator
   mrc = compute_MRC(df_prices, n_assets, kn)
-  #mrc = rMRCov(price_list)
+  # mrc = rMRCov(price_list)
 
   # The asymptotic covariance matrix
   ublb = matrix(create_ublb(n_assets, df_prices, kn), nrow = nrow(mrc), byrow = TRUE)
+  
+  ublb = abs(ublb)
   
   IV = make_IV(sigma_list, n_assets)
   
@@ -70,17 +53,39 @@ sim_study = function(n_assets){
 
   mse_matrix = (mrc - IV)
 
-  return(list("TF_sum" = TF_sum, "std_norm" = std_norm, "MSE" = mse_matrix, "MRC" = mrc, "QV" = IV))
+  return(
+    list(
+      "TF_sum" = TF_sum, 
+      "std_norm" = std_norm, 
+      "MSE" = mse_matrix, 
+      "MRC" = mrc, 
+      "QV" = IV,
+      "bounds" = ublb
+    )
+  )
 }
 
+# sim_list = pbmclapply(rep(5, 100), MRC_monte_carlo, mc.cores = 8)
 
-sim_list = pbmclapply(rep(5, 100), sim_study, mc.cores = 8)
+seeds = 401:600
 
-TF_sum_list = purrr::map(sim_list, .f = 1)
-std_norm_list = purrr::map(sim_list, .f = 2)
-MSE_list = purrr::map(sim_list, .f = 3)
+future::plan(future::multisession(), workers = future::availableCores() - 2)
+tictoc::tic()
+sim_list = furrr::future_map(
+  .x = seeds, 
+  .f = MRC_monte_carlo, 
+  .options = furrr::furrr_options(seed = TRUE),
+  .progress = TRUE
+)
+tictoc::toc()
 
-sum(unlist(TF_sum_list), na.rm = TRUE)/2500
+purrr::map(sim_list, .f = "bounds")
+
+TF_sum = purrr::map_dbl(sim_list, .f = "TF_sum")
+std_norm_list = purrr::map(sim_list, .f = "std_norm")
+MSE_list = purrr::map(sim_list, .f = "MSE")
+
+sum(TF_sum, na.rm = TRUE)/(n_assets*n_assets*length(seeds))
 
 hist(sapply(std_norm_list, "[[", 1), breaks = 100)
 # -------------------------------------------------------------------------
