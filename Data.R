bcb8583ddd38fb8e4facaaf20d796d89d2ecdde7
josandan <josandanr@gmail.com>
2022-12-09 09:38:49 +0100

    data updated to 6 months

--- Data.R
+++ Data.R
@@ -1,18 +1,21 @@
-# library(furrr)
+library(furrr)
 library(data.table)
 library(tidyverse)
 library(highfrequency)
+library(tictoc)
 source("functions/get_data_df.R")
 
-future::plan(future::multisession(), workers = future::availableCores() - 2)
-tictoc::tic()
+future::plan(future::multisession(), workers = 40)
 data = get_data_df()
-tictoc::toc()
+
+log_data = purrr::map(.x = data, .f = function(x) {
+  x %>% 
+    mutate(Price = log(Price))
+})
 
 asset_names = names(data)
 n_assets = length(asset_names)
 theta = 0.8
-gamma = 0.001
 R.utils::sourceDirectory("functions", modifiedOnly = FALSE)
 
 get_MRC_by_date = function(date, data, n_assets) {
@@ -26,13 +29,107 @@ get_MRC_by_date = function(date, data, n_assets) {
     compute_MRC_data(n_assets = n_assets)
 }
 
-date_seq = data$AUDNZD$DT %>% as.Date() %>% unique()
+date_seq = data$AUDNZD$DT %>% as.Date(tz = "CET") %>% unique()
 
+tic()
 MRC_series = furrr::future_map(
   .x = date_seq %>% setNames(date_seq),
   .f = get_MRC_by_date,
-  data = data,
+  data = log_data,
   n_assets = n_assets,
-  .options = furrr::furrr_options(seed = TRUE)
+  .options = furrr::furrr_options(seed = TRUE),
+  .progress = TRUE
 )
+toc()
+
+compute_daily_returns = function(df, date_seq){
+  ref = data.table("DT" = as.POSIXct(date_seq) - lubridate::hours(1))
+  returns = setDT(as.data.table(df))[ref, Price, roll = "nearest", on = "DT"] %>% 
+    diff()
+}
+
+daily_returns = purrr::map_dfc(
+  .x = log_data, 
+  .f = compute_daily_returns, 
+  date_seq = date_seq
+) %>% 
+  # mutate(DT = as.POSIXct(date_seq[-1]), .before = 1) %>% 
+  as.data.frame()
+
+
+library(rugarch)
+library(rmgarch)
+
+uspec_n = multispec(replicate(n_assets, ugarchspec(
+  mean.model = list(armaOrder = c(0,0)),
+  variance.model = list(model = "sGARCH", garchOrder = c(1,1))
+)))
+multf = multifit(uspec_n, daily_returns)
+
+spec1 = dccspec(uspec = uspec_n, dccOrder = c(1, 1), distribution = 'mvnorm')
+
+fit1 = dccfit(
+  spec1, 
+  data = daily_returns, 
+  fit.control = list(eval.se = TRUE), 
+  fit = multf
+)
+fit2 = dccfit(
+  spec1, 
+  data = daily_returns, 
+  fit.control = list(eval.se = TRUE), 
+  fit = multf,
+  realizedVol = stocks[1:128,]
+)
+
+pred1 = dccforecast(fit1, n.ahead = 1)
+fitted(pred1)
+sigma(pred1)
+rcov(pred1)
+rcor(pred1)
+
+
+
+quantmod::getSymbols("IBM", from = as.Date("2022-01-01"), to = as.Date("2022-08-01"))
+rIBM = quantmod::dailyReturn(IBM)
+rGOOG = quantmod::dailyReturn(GOOG)
+
+stocks = data.frame(xts(rIBM), xts(rGOOG))
+test$daily.returns %>% class()
+
+
+# get weights
+
+mrc = MRC_series$`2018-06-29`
+mrc_inv = solve(mrc)
+weigth_matrix = (mrc_inv %*% rep(1, n_assets)) / 
+  as.double(t(rep(1, n_assets)) %*% mrc_inv %*% rep(1, n_assets))
+weigths = as.vector(weigth_matrix) %>% setNames(rownames(weigth_matrix))
+
+quadprog::solve.QP(
+  Dmat = mrc, 
+  Amat = matrix(rep(1, n_assets*n_assets), ncol = 10, byrow = T), 
+  bvec = rep(1, n_assets), 
+  dvec = rep(0, n_assets),
+  meq = 0
+)
+
+pred_returns = as.vector(fitted(pred1))
+target_return = 0.1
+
+sol = quadprog::solve.QP(
+  Dmat = mrc, 
+  Amat = matrix(c(pred_returns, rep(1, (n_assets-1)*n_assets)), ncol = 10, byrow = T), 
+  bvec = c(target_return, rep(1, n_assets-1)), 
+  dvec = rep(0, n_assets),
+  meq = 0
+)$solution
+
+pred_returns %*% sol
+
+
+
+
+
+
 
--- functions/get_data_df.R
+++ functions/get_data_df.R
@@ -1,181 +1,110 @@
 get_data_df = function(){
-  # AUDCAD= read.csv("Data/DAT_NT_AUDCAD_T_LAST_201801.csv", sep=";", header = FALSE)
-  # 
-  # AUDCHF= read.csv("Data/DAT_NT_AUDCHF_T_LAST_201801.csv", sep=";", header = FALSE)
-  # 
-  # AUDJPY= read.csv("Data/DAT_NT_AUDJPY_T_LAST_201801.csv", sep=";", header = FALSE)
-  # 
-  # AUDNZD= read.csv("Data/DAT_NT_AUDNZD_T_LAST_201801.csv", sep=";", header = FALSE)
-  # 
-  # AUDUSD= read.csv("Data/DAT_NT_AUDUSD_T_LAST_201801.csv", sep=";", header = FALSE)
-  # 
-  # CADCHF= read.csv("Data/DAT_NT_CADCHF_T_LAST_201801.csv", sep=";", header = FALSE)
-  # 
-  # CADJPY= read.csv("Data/DAT_NT_CADJPY_T_LAST_201801.csv", sep=";", header = FALSE)
-  # 
-  # CHFJPY= read.csv("Data/DAT_NT_CHFJPY_T_LAST_201801.csv", sep=";", header = FALSE)
-  # 
-  # EURAUD= read.csv("Data/DAT_NT_EURAUD_T_LAST_201801.csv", sep=";", header = FALSE)
-  # 
-  # EURCAD= read.csv("Data/DAT_NT_EURCAD_T_LAST_201801.csv", sep=";", header = FALSE)
-  # 
-  # EURCHF= read.csv("Data/DAT_NT_EURCHF_T_LAST_201801.csv", sep=";", header = FALSE)
-  # 
-  # EURCZK= read.csv("Data/DAT_NT_EURCZK_T_LAST_201801.csv", sep=";", header = FALSE)
-  # 
-  # EURGBP= read.csv("Data/DAT_NT_EURGBP_T_LAST_201801.csv", sep=";", header = FALSE)
-  # 
-  # EURHUF= read.csv("Data/DAT_NT_EURHUF_T_LAST_201801.csv", sep=";", header = FALSE)
-  # 
-  # EURJPY= read.csv("Data/DAT_NT_EURJPY_T_LAST_201801.csv", sep=";", header = FALSE)
-  # 
-  # EURNOK= read.csv("Data/DAT_NT_EURNOK_T_LAST_201801.csv", sep=";", header = FALSE)
-  # 
-  # EURNZD= read.csv("Data/DAT_NT_EURNZD_T_LAST_201801.csv", sep=";", header = FALSE)
-  # 
-  # EURPLN= read.csv("Data/DAT_NT_EURPLN_T_LAST_201801.csv", sep=";", header = FALSE)
-  # 
-  # EURSEK= read.csv("Data/DAT_NT_EURSEK_T_LAST_201801.csv", sep=";", header = FALSE)
-  # 
-  # EURTRY= read.csv("Data/DAT_NT_EURTRY_T_LAST_201801.csv", sep=";", header = FALSE)
-  # 
-  # EURUSD= read.csv("Data/DAT_NT_EURUSD_T_LAST_201801.csv", sep=";", header = FALSE)
-  # 
-  # GBPAUD= read.csv("Data/DAT_NT_GBPAUD_T_LAST_201801.csv", sep=";", header = FALSE)
-  # 
-  # GBPCAD= read.csv("Data/DAT_NT_GBPCAD_T_LAST_201801.csv", sep=";", header = FALSE)
-  # 
-  # GBPCHF= read.csv("Data/DAT_NT_GBPCHF_T_LAST_201801.csv", sep=";", header = FALSE)
-  # 
-  # GBPJPY= read.csv("Data/DAT_NT_GBPJPY_T_LAST_201801.csv", sep=";", header = FALSE)
-  # 
-  # GBPNZD= read.csv("Data/DAT_NT_GBPNZD_T_LAST_201801.csv", sep=";", header = FALSE)
-  # 
-  # GBPUSD= read.csv("Data/DAT_NT_GBPUSD_T_LAST_201801.csv", sep=";", header = FALSE)
-  # 
-  # NZDCAD= read.csv("Data/DAT_NT_NZDCAD_T_LAST_201801.csv", sep=";", header = FALSE)
-  # 
-  # NZDCHF= read.csv("Data/DAT_NT_NZDCHF_T_LAST_201801.csv", sep=";", header = FALSE)
-  # 
-  # NZDJPY= read.csv("Data/DAT_NT_NZDJPY_T_LAST_201801.csv", sep=";", header = FALSE)
-  # 
-  # NZDUSD= read.csv("Data/DAT_NT_NZDUSD_T_LAST_201801.csv", sep=";", header = FALSE)
-  # 
-  # SGDJPY= read.csv("Data/DAT_NT_SGDJPY_T_LAST_201801.csv", sep=";", header = FALSE)
-  # 
-  # USDCAD= read.csv("Data/DAT_NT_USDCAD_T_LAST_201801.csv", sep=";", header = FALSE)
-  # 
-  # USDCHF= read.csv("Data/DAT_NT_USDCHF_T_LAST_201801.csv", sep=";", header = FALSE)
-  # 
-  # USDCZK= read.csv("Data/DAT_NT_USDCZK_T_LAST_201801.csv", sep=";", header = FALSE)
-  # 
-  # USDHUF= read.csv("Data/DAT_NT_USDHUF_T_LAST_201801.csv", sep=";", header = FALSE)
-  # 
-  # USDJPY= read.csv("Data/DAT_NT_USDJPY_T_LAST_201801.csv", sep=";", header = FALSE)
-  # 
-  # USDNOK= read.csv("Data/DAT_NT_USDNOK_T_LAST_201801.csv", sep=";", header = FALSE)
-  # 
-  # USDPLN= read.csv("Data/DAT_NT_USDPLN_T_LAST_201801.csv", sep=";", header = FALSE)
-  # 
-  # USDSEK= read.csv("Data/DAT_NT_USDSEK_T_LAST_201801.csv", sep=";", header = FALSE)
-  # 
-  # USDSGD= read.csv("Data/DAT_NT_USDSGD_T_LAST_201801.csv", sep=";", header = FALSE)
-  # 
-  # USDTRY= read.csv("Data/DAT_NT_USDTRY_T_LAST_201801.csv", sep=";", header = FALSE)
-  # 
-  # USDZAR= read.csv("Data/DAT_NT_USDZAR_T_LAST_201801.csv", sep=";", header = FALSE)
-  # 
-  # ZARJPY= read.csv("Data/DAT_NT_ZARJPY_T_LAST_201801.csv", sep=";", header = FALSE)
   
-  # DF = list(
-  #   "AUDCAD" = AUDCAD,
-  #   "AUDCHF" = AUDCHF,
-  #   "AUDJPY" = AUDJPY,
-  #   "AUDNZD" = AUDNZD,
-  #   "AUDUSD" = AUDUSD,
-  #   "CADCHF" = CADCHF,
-  #   "CADJPY" = CADJPY,
-  #   "CHFJPY" = CHFJPY,
-  #   "EURAUD" = EURAUD,
-  #   "EURCAD" = EURCAD,
-  #   "EURCHF" = EURCHF,
-  #   "EURCZK" = EURCZK,
-  #   "EURGBP" = EURGBP,
-  #   "EURHUF" = EURHUF,
-  #   "EURJPY" = EURJPY,
-  #   "EURNOK" = EURNOK,
-  #   "EURNZD" = EURNZD,
-  #   "EURPLN" = EURPLN,
-  #   "EURSEK" = EURSEK,
-  #   "EURTRY" = EURTRY,
-  #   "EURUSD" = EURUSD,
-  #   "GBPAUD" = GBPAUD,
-  #   "GBPCAD" = GBPCAD,
-  #   "GBPJPY" = GBPJPY,
-  #   "GBPNZD" = GBPNZD,
-  #   "GBPUSD" = GBPUSD,
-  #   "NZDCAD" = NZDCAD,
-  #   "NZDCHF" = NZDCHF,
-  #   "NZDJPY" = NZDJPY,
-  #   "NZDUSD" = NZDUSD,
-  #   "SGDJPY" = SGDJPY,
-  #   "USDCAD" = USDCAD,
-  #   "USDCHF" = USDCHF,
-  #   "USDCZK" = USDCZK,
-  #   "USDHUF" = USDHUF,
-  #   "USDJPY" = USDJPY,
-  #   "USDNOK" = USDNOK,
-  #   "USDPLN" = USDPLN,
-  #   "USDSEK" = USDSEK,
-  #   "USDSGD" = USDSGD,
-  #   "USDTRY" = USDTRY,
-  #   "ZARJPY" = ZARJPY
-  # )
-  
+  tictoc::tic()
   
   # selected assets
-  AUDNZD = read.csv("Data/DAT_NT_AUDNZD_T_LAST_201801.csv", sep=";", header = FALSE)
-  EURNOK = read.csv("Data/DAT_NT_EURNOK_T_LAST_201801.csv", sep=";", header = FALSE)
-  EURPLN = read.csv("Data/DAT_NT_EURPLN_T_LAST_201801.csv", sep=";", header = FALSE)
-  EURUSD = read.csv("Data/DAT_NT_EURUSD_T_LAST_201801.csv", sep=";", header = FALSE)
-  GBPJPY = read.csv("Data/DAT_NT_GBPJPY_T_LAST_201801.csv", sep=";", header = FALSE)
-  GBPUSD = read.csv("Data/DAT_NT_GBPUSD_T_LAST_201801.csv", sep=";", header = FALSE)
-  NZDCAD = read.csv("Data/DAT_NT_NZDCAD_T_LAST_201801.csv", sep=";", header = FALSE)
-  SGDJPY = read.csv("Data/DAT_NT_SGDJPY_T_LAST_201801.csv", sep=";", header = FALSE)
-  USDCHF = read.csv("Data/DAT_NT_USDCHF_T_LAST_201801.csv", sep=";", header = FALSE)
-  ZARJPY = read.csv("Data/DAT_NT_ZARJPY_T_LAST_201801.csv", sep=";", header = FALSE)
-  
-  selected_assets = list(
-    "AUDNZD" = AUDNZD,
-    "EURNOK" = EURNOK,
-    "EURPLN" = EURPLN,
-    "EURUSD" = EURUSD,
-    "GBPJPY" = GBPJPY,
-    "GBPUSD" = GBPUSD,
-    "NZDCAD" = NZDCAD,
-    "SGDJPY" = SGDJPY,
-    "USDCHF" = USDCHF,
-    "ZARJPY" = ZARJPY
+  assets = list(
+    "AUDNZD" = c(
+      "Data/DAT_NT_AUDNZD_T_LAST_201801.csv",
+      "Data/DAT_NT_AUDNZD_T_LAST_201802.csv",
+      "Data/DAT_NT_AUDNZD_T_LAST_201803.csv",
+      "Data/DAT_NT_AUDNZD_T_LAST_201804.csv",
+      "Data/DAT_NT_AUDNZD_T_LAST_201805.csv",
+      "Data/DAT_NT_AUDNZD_T_LAST_201806.csv"
+    ),
+    "EURNOK" = c(
+      "Data/DAT_NT_EURNOK_T_LAST_201801.csv",
+      "Data/DAT_NT_EURNOK_T_LAST_201802.csv",
+      "Data/DAT_NT_EURNOK_T_LAST_201803.csv",
+      "Data/DAT_NT_EURNOK_T_LAST_201804.csv",
+      "Data/DAT_NT_EURNOK_T_LAST_201805.csv",
+      "Data/DAT_NT_EURNOK_T_LAST_201806.csv"
+    ),
+    "EURPLN" = c(
+      "Data/DAT_NT_EURPLN_T_LAST_201801.csv",
+      "Data/DAT_NT_EURPLN_T_LAST_201802.csv",
+      "Data/DAT_NT_EURPLN_T_LAST_201803.csv",
+      "Data/DAT_NT_EURPLN_T_LAST_201804.csv",
+      "Data/DAT_NT_EURPLN_T_LAST_201805.csv",
+      "Data/DAT_NT_EURPLN_T_LAST_201806.csv"
+    ),
+    "EURUSD" = c(
+      "Data/DAT_NT_EURUSD_T_LAST_201801.csv",
+      "Data/DAT_NT_EURUSD_T_LAST_201802.csv",
+      "Data/DAT_NT_EURUSD_T_LAST_201803.csv",
+      "Data/DAT_NT_EURUSD_T_LAST_201804.csv",
+      "Data/DAT_NT_EURUSD_T_LAST_201805.csv",
+      "Data/DAT_NT_EURUSD_T_LAST_201806.csv"
+    ),
+    "GBPJPY" = c(
+      "Data/DAT_NT_GBPJPY_T_LAST_201801.csv",
+      "Data/DAT_NT_GBPJPY_T_LAST_201802.csv",
+      "Data/DAT_NT_GBPJPY_T_LAST_201803.csv",
+      "Data/DAT_NT_GBPJPY_T_LAST_201804.csv",
+      "Data/DAT_NT_GBPJPY_T_LAST_201805.csv",
+      "Data/DAT_NT_GBPJPY_T_LAST_201806.csv"
+    ),
+    "GBPUSD" = c(
+      "Data/DAT_NT_GBPUSD_T_LAST_201801.csv",
+      "Data/DAT_NT_GBPUSD_T_LAST_201802.csv",
+      "Data/DAT_NT_GBPUSD_T_LAST_201803.csv",
+      "Data/DAT_NT_GBPUSD_T_LAST_201804.csv",
+      "Data/DAT_NT_GBPUSD_T_LAST_201805.csv",
+      "Data/DAT_NT_GBPUSD_T_LAST_201806.csv"
+    ),
+    "NZDCAD" = c(
+      "Data/DAT_NT_NZDCAD_T_LAST_201801.csv",
+      "Data/DAT_NT_NZDCAD_T_LAST_201802.csv",
+      "Data/DAT_NT_NZDCAD_T_LAST_201803.csv",
+      "Data/DAT_NT_NZDCAD_T_LAST_201804.csv",
+      "Data/DAT_NT_NZDCAD_T_LAST_201805.csv",
+      "Data/DAT_NT_NZDCAD_T_LAST_201806.csv"
+    ),
+    "SGDJPY" = c(
+      "Data/DAT_NT_SGDJPY_T_LAST_201801.csv",
+      "Data/DAT_NT_SGDJPY_T_LAST_201802.csv",
+      "Data/DAT_NT_SGDJPY_T_LAST_201803.csv",
+      "Data/DAT_NT_SGDJPY_T_LAST_201804.csv",
+      "Data/DAT_NT_SGDJPY_T_LAST_201805.csv",
+      "Data/DAT_NT_SGDJPY_T_LAST_201806.csv"
+    ),
+    "USDCHF" = c(
+      "Data/DAT_NT_USDCHF_T_LAST_201801.csv",
+      "Data/DAT_NT_USDCHF_T_LAST_201802.csv",
+      "Data/DAT_NT_USDCHF_T_LAST_201803.csv",
+      "Data/DAT_NT_USDCHF_T_LAST_201804.csv",
+      "Data/DAT_NT_USDCHF_T_LAST_201805.csv",
+      "Data/DAT_NT_USDCHF_T_LAST_201806.csv"
+    ),
+    "ZARJPY" = c(
+      "Data/DAT_NT_ZARJPY_T_LAST_201801.csv",
+      "Data/DAT_NT_ZARJPY_T_LAST_201802.csv",
+      "Data/DAT_NT_ZARJPY_T_LAST_201803.csv",
+      "Data/DAT_NT_ZARJPY_T_LAST_201804.csv",
+      "Data/DAT_NT_ZARJPY_T_LAST_201805.csv",
+      "Data/DAT_NT_ZARJPY_T_LAST_201806.csv"
+    )
   )
   
-  #Log af DF
-  #for (i in 1:length(DF)) {
-  #DF[[i]][[2]] <- log(DF[[i]][[2]])
-  # i=i+1
-  #}
+  data = furrr::future_map(
+    .x = assets,
+    .f = function(x) {
+      purrr::map_dfr(.x = x, .f = read.csv, sep = ";", header = FALSE) %>% 
+        dplyr::mutate(
+          V1 = as.POSIXct(V1, format= "%Y%m%d %H%M%S") + lubridate::hours(7)
+        ) %>% 
+          dplyr::select(DT = V1, Price = V2) %>% 
+          # dplyr::filter(as.Date(DT)==as.Date("2018-01-01")) %>% 
+          as_tibble()
+        # data.table()
+    },
+    .progress = TRUE
+  )
   
-  ###Mutate datoer, vælger to første vektorer, samt vælger første dag i datasættet
-  mutate_data = function(data){
-    data %>% dplyr::mutate(
-      V1 = as.POSIXct(V1, format= "%Y%m%d %H%M%S")
-    ) %>% 
-      dplyr::select(DT = V1, Price = V2) %>% 
-      # dplyr::filter(as.Date(DT)==as.Date("2018-01-01")) %>% 
-      as_tibble()
-      # data.table()
-  }
+  cat("\n")
+  tictoc::toc()
   
-  furrr::future_map(selected_assets, mutate_data)
+  return(data)
 }
 
 compute_MRC_data = function(df_prices, n_assets)
